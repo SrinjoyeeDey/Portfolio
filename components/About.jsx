5f051616e15944e1dc11ac5aa95d678127922aff
Srinjoyee_Dey <deysrinjoyee28@gmail.com>
2025-08-31 17:01:38 +0530

    issue fix

--- components/About.jsx
+++ components/About.jsx
@@ -21,14 +21,15 @@ const About = () => {
             </p>
 
             <ul className='grid grid-cols-1 sm:grid-cols-3 gap-6 max-w-2xl'>
+              
                 {infoList.map(({icon,title,link,description},index)=>(
-                  <a href={link }>
+                  
                     <li className='border-[0.5px] border-gray-400 rounded-xl p-6 cursor-pointer hover:bg-pink-100 hover:-translate-y-1 duration-500 hover:shadow-lg shadow-black' key={index}>
                         <Image src={icon} alt={title} className='w-7 mt-3'/>
                         <h3 className='my-4 font-semibold text-gray-700'>{title}</h3>
                         <p className='text-gray-600 text-sm'>{description}</p>
                     </li>
-                    </a>
+                   
                 ))}
             </ul>
             
